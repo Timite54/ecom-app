1547f6cdc82e645eec4699ed05e73bb48359797e
Timite54 <kadernaho@gmail.com>
2024-11-06 19:13:48 +0000

    projection sur le nom et le email

--- customer-service/src/main/java/ci/luckyman/customerservice/customer/CustomerProjection.java
+++ customer-service/src/main/java/ci/luckyman/customerservice/customer/CustomerProjection.java
@@ -1,4 +1,10 @@
 package ci.luckyman.customerservice.customer;
 
+import lombok.Getter;
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(name = "all", types = Customer.class)
 public interface CustomerProjection {
+    public String getName();
+    public String getEmail();
 }
--- customer-service/src/main/java/ci/luckyman/customerservice/customer/CustomerProjectionEmail.java
+++ customer-service/src/main/java/ci/luckyman/customerservice/customer/CustomerProjectionEmail.java
@@ -1,4 +1,8 @@
 package ci.luckyman.customerservice.customer;
 
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(name = "email", types = Customer.class)
 public interface CustomerProjectionEmail {
+    public String getEmail();
 }

319e342581a7f8dff6a735bde3d3a4578fc13f1e
Timite54 <kadernaho@gmail.com>
2024-11-10 13:54:03 +0000

    création et config de discovery pour que les ms s'enregistre auptrès de lui

--- gateway-service/src/main/java/ci/luckyman/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/ci/luckyman/gatewayservice/GatewayServiceApplication.java
@@ -1,7 +1,9 @@
 package ci.luckyman.gatewayservice;
 
+import jdk.jfr.Enabled;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class GatewayServiceApplication {
